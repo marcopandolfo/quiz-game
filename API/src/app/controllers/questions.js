ff4f5fd1b2d6060a06593eb7ab344537633fb921
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-15 21:29:30 -0300

    Melhoria na resp do POST

--- API/src/app/controllers/questions.js
+++ API/src/app/controllers/questions.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
         return new app.infra.QuestionsDao(connection);
     };
 
+    // GET
     app.get('/questions', (req, res) => {
         getQuestionsDao().getRandom((err, result) => {
             if (err) return res.status(500).json(err);
@@ -11,10 +12,23 @@ module.exports = (app) => {
         });
     });
 
+    // POST
     app.post('/questions', (req, res) => {
-        getQuestionsDao().saveQuestion(req.body, (err, result) => {
+        const question = {
+            question: req.body.question,
+            correct_answer: req.body.correct_answer,
+            category: req.body.category,
+            incorrect_answers: req.body.incorrect_answers,
+        };
+
+        getQuestionsDao().saveQuestion(question, (err, result) => {
             if (err) return res.status(500).json(err);
-            return res.status(200).json(result);
+
+            const info = {
+                status: 'CREATED',
+                insertId: result.insertId,
+            };
+            return res.status(200).json({ question, info });
         });
     });
 };
