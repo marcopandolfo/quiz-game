6d3487d64891cb4f190d961aeb29104371f3a7c7
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-05-15 21:31:11 -0300

    Método para deletar question

--- API/src/app/infra/questionsDao.js
+++ API/src/app/infra/questionsDao.js
@@ -15,6 +15,10 @@ QuestionsDao.prototype.getRandom = function getRandom(callback) {
     this._connection.query('SELECT * FROM questions ORDER BY RAND() LIMIT 1', callback);
 };
 
+QuestionsDao.prototype.deleteQuestion = function deleteQuestion(id, callback) {
+    this._connection.query('DELETE * FROM questions WHERE ID= ?', id, callback);
+};
+
 // eslint-disable-next-line func-names
 module.exports = function () {
     return QuestionsDao;
