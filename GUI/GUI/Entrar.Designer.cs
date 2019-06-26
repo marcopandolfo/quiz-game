4bc7595b474b7ec7e5a95abe4e4301d7c27137cc
m7Aei <mateuspm2015@hotmail.com>
2019-06-26 00:49:19 -0300

    Adicionando a janela para logar na conta

---
+++ GUI/GUI/Entrar.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace GUI
+{
+    partial class Entrar
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Entrar));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bttnLogin = new System.Windows.Forms.Button();
+            this.bttnRegister = new System.Windows.Forms.Button();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.bttnMinimize = new System.Windows.Forms.Button();
+            this.bttnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Fira Code", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.label1.Location = new System.Drawing.Point(132, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ENTRE NA SUA CONTA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Fira Code", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(126, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "   Senha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Fira Code", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(133, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "   Login:";
+            // 
+            // bttnLogin
+            // 
+            this.bttnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.bttnLogin.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bttnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnLogin.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnLogin.ForeColor = System.Drawing.Color.White;
+            this.bttnLogin.Location = new System.Drawing.Point(131, 377);
+            this.bttnLogin.Name = "bttnLogin";
+            this.bttnLogin.Size = new System.Drawing.Size(165, 44);
+            this.bttnLogin.TabIndex = 3;
+            this.bttnLogin.Text = "Login";
+            this.bttnLogin.UseVisualStyleBackColor = false;
+            // 
+            // bttnRegister
+            // 
+            this.bttnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.bttnRegister.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.bttnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnRegister.Font = new System.Drawing.Font("Fira Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnRegister.ForeColor = System.Drawing.Color.White;
+            this.bttnRegister.Location = new System.Drawing.Point(322, 377);
+            this.bttnRegister.Name = "bttnRegister";
+            this.bttnRegister.Size = new System.Drawing.Size(165, 44);
+            this.bttnRegister.TabIndex = 4;
+            this.bttnRegister.Text = "Register";
+            this.bttnRegister.UseVisualStyleBackColor = false;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(137, 171);
+            this.txtLogin.Multiline = true;
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(349, 33);
+            this.txtLogin.TabIndex = 5;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(131, 287);
+            this.txtSenha.Multiline = true;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(355, 33);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // bttnMinimize
+            // 
+            this.bttnMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.bttnMinimize.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bttnMinimize.BackgroundImage")));
+            this.bttnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bttnMinimize.FlatAppearance.BorderSize = 0;
+            this.bttnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnMinimize.Location = new System.Drawing.Point(543, 19);
+            this.bttnMinimize.Name = "bttnMinimize";
+            this.bttnMinimize.Size = new System.Drawing.Size(49, 35);
+            this.bttnMinimize.TabIndex = 7;
+            this.bttnMinimize.UseVisualStyleBackColor = false;
+            this.bttnMinimize.Click += new System.EventHandler(this.BttnMinimize_Click);
+            // 
+            // bttnClose
+            // 
+            this.bttnClose.BackColor = System.Drawing.Color.Transparent;
+            this.bttnClose.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bttnClose.BackgroundImage")));
+            this.bttnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.bttnClose.FlatAppearance.BorderSize = 0;
+            this.bttnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnClose.Location = new System.Drawing.Point(598, 19);
+            this.bttnClose.Name = "bttnClose";
+            this.bttnClose.Size = new System.Drawing.Size(49, 35);
+            this.bttnClose.TabIndex = 8;
+            this.bttnClose.UseVisualStyleBackColor = false;
+            this.bttnClose.Click += new System.EventHandler(this.BttnClose_Click);
+            // 
+            // Entrar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(659, 550);
+            this.Controls.Add(this.bttnClose);
+            this.Controls.Add(this.bttnMinimize);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.bttnRegister);
+            this.Controls.Add(this.bttnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Entrar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bttnLogin;
+        private System.Windows.Forms.Button bttnRegister;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button bttnMinimize;
+        private System.Windows.Forms.Button bttnClose;
+    }
+}
+
---
+++ GUI/GUI/Entrar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Entrar : Form
+    {
+        public Entrar()
+        {
+            InitializeComponent();
+        }
+
+        private void BttnClose_Click(object sender, EventArgs e)
+        {
+            Entrar.ActiveForm.Close();
+        }
+
+        private void BttnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
