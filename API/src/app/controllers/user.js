b602b7a631b41345677cfb978feeb99e9c9e80d2
m7Aei <mateuspm2015@hotmail.com>
2019-06-29 15:50:39 -0300

    Fazendo metodos no controller do usuario

--- API/src/app/controllers/user.js
+++ API/src/app/controllers/user.js
@@ -1,1 +1,25 @@
-const Question = require("../models/user");
+const User = require("../models/user");
+
+module.exports = app => {
+  // POST
+  app.post("/users", (req, res) => {
+    // Validate
+    const errors = User.validade(req.body);
+    if (errors.length !== 0) return res.status(400).send({ errors });
+
+    const user = new User(
+      req.body.username,
+      req.body.email,
+      req.body.password,
+      req.body.confirmPassword
+    );
+
+    // Save to DB
+  });
+
+  // DELETE
+  app.delete("/users/:id", (req, res) => {
+    if (isNaN(req.params.id))
+      return res.status(400).send({ errors: ["invalid ID"] });
+  });
+};
