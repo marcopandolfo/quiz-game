ef12fb850935f1c97ae2158a70567c98d30aaec7
Marco Antonio Pandolfo <marcopandolfo577@gmail.com>
2019-06-30 22:58:11 -0300

    Query sql para criar as tabelas

---
+++ API/util/tables/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int(10) NOT NULL auto_increment,
+	`username` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	PRIMARY KEY( `id` )
+);
