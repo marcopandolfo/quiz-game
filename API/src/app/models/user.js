5b5ee9854e4b23b0488ddab0da4b1e7f0fd87b49
m7Aei <mateuspm2015@hotmail.com>
2019-07-01 17:16:55 -0300

    Adicionando a parte de hash no model de usuario

--- API/src/app/models/user.js
+++ API/src/app/models/user.js
@@ -1,19 +1,31 @@
+// const bcrypt = require("bcryptjs");
+
 class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  static validade(body) {
+    const errors = [];
+
+    if (!body.username) errors.push("username can not be empty");
+    if (!body.email) errors.push("email can not be empty");
+    if (!body.password) errors.push("password can not be empty");
+
+    return errors;
+  }
+  /* , {
+        hooks: {
+            beforeSave: async user => {
+                if (user.password) {
+                    user.password = await bcrypt.hash(user.password, 8)
+                }
+            }
+        }
     }
-
-    static validade(body) {
-        const errors = [];
-
-        if (!body.username) errors.push('username can not be empty');
-        if (!body.email) errors.push('email can not be empty');
-        if (!body.password) errors.push('password can not be empty');
-
-        return errors;
-    }
+  */
 }
 
 module.exports = User;
