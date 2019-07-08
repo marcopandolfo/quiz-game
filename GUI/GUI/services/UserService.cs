5c680a7fe33950ac33c29169aba445909bbc0d7b
m7Aei <mateuspm2015@hotmail.com>
2019-07-08 01:26:53 -0300

    Adicionando a parte de GET dos users

--- GUI/GUI/services/UserService.cs
+++ GUI/GUI/services/UserService.cs
@@ -22,5 +22,16 @@ namespace GUI.services
             request.AddJsonBody(json);
             client.Execute(request);
         }
+
+        // GET
+        static public User SearchUser(string login, string password)
+        {
+            var client = new RestClient(@"http://localhost:3000/users");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Accept", "application/json");
+            IRestResponse response = client.Execute(request);
+            var user = JsonConvert.DeserializeObject<User>(response.Content);
+            return user;
+        }
     }
 }
