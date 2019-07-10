e22c59e40138174ee36e70d5fb68e86e03c5b6e7
m7Aei <mateuspm2015@hotmail.com>
2019-07-09 21:10:37 -0300

    Passando o DAO de users para Promises

--- API/src/app/infra/UserDAO.js
+++ API/src/app/infra/UserDAO.js
@@ -3,16 +3,37 @@ function UserDAO(connection) {
   this._connection = connection;
 }
 
-UserDAO.prototype.saveUser = function saveUser(users, callbak) {
-  this._connection.query('INSERT INTO users SET ?', users, callbak);
+// Salvar um user
+UserDAO.prototype.saveUser = function saveUser() {
+  return new Promise((resolve, reject) => {
+    this._connection.query('INSERT INTO users SET ?', (err, result) => {
+      if (err) return reject(err);
+
+      return resolve(result);
+    });
+  });
 };
 
-UserDAO.prototype.deleteUser = function deleteUser(id, callbak) {
-  this._connection.query('DELETE FROM users WHERE ID = ?', [id], callbak);
+// Pegar algum user
+UserDAO.prototype.getUser = function getUser() {
+  return new Promise((resolve, reject) => {
+    this._connection.query('SELECT * FROM users WHERE email=? AND password=?', (err, result) => {
+      if (err) return reject(err);
+
+      return resolve(result);
+    });
+  });
 };
 
-UserDAO.prototype.getUser = function getUser(user, callbak) {
-  this._connection.query('SELECT * FROM users WHERE email=? AND password=?', [user.email, user.password], callbak);
+// Deletar um user
+UserDAO.prototype.deleteUser = function deleteUser() {
+  return new Promise((resolve, reject) => {
+    this._connection.query('DELETE FROM users WHERE ID = ?', (err, result) => {
+      if (err) return reject(err);
+
+      return resolve(result);
+    });
+  });
 };
 
 // eslint-disable-next-line func-names
