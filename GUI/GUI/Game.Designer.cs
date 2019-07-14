5b971cf16bdd8e2e31f895a676adf176d4033b45
m7Aei <mateuspm2015@hotmail.com>
2019-07-14 20:03:28 -0300

    Adicionando duas labels no GUI

--- GUI/GUI/Game.Designer.cs
+++ GUI/GUI/Game.Designer.cs
@@ -39,6 +39,8 @@
             this.mainLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -161,9 +163,9 @@
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
             this.label3.Location = new System.Drawing.Point(72, 76);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 74);
+            this.label3.Size = new System.Drawing.Size(118, 74);
             this.label3.TabIndex = 16;
-            this.label3.Text = "Rodada:  0";
+            this.label3.Text = "Rodada:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
@@ -173,12 +175,40 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
-            this.label2.Location = new System.Drawing.Point(674, 76);
+            this.label2.Location = new System.Drawing.Point(665, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(181, 74);
             this.label2.TabIndex = 17;
-            this.label2.Text = "Pontuação:  0";
+            this.label2.Text = "Pontuação:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AllowDrop = true;
+            this.label4.AutoEllipsis = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.label4.Location = new System.Drawing.Point(184, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 74);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "0";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AllowDrop = true;
+            this.label5.AutoEllipsis = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(62)))), ((int)(((byte)(209)))));
+            this.label5.Location = new System.Drawing.Point(826, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 74);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Game
             // 
@@ -187,6 +217,8 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(928, 687);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.mainLabel);
@@ -219,5 +251,7 @@
         private System.Windows.Forms.Label mainLabel;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
