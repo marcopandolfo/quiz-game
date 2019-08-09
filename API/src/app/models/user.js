2a60e3c980e9c8d0dac8ed14563502e966226db6
m7Aei <mateuspm2015@hotmail.com>
2019-08-09 14:09:18 -0300

    Tentando fazer a parte de hash de senha do user0

--- API/src/app/models/user.js
+++ API/src/app/models/user.js
@@ -1,31 +1,32 @@
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 
 class User {
-  constructor(username, email, password) {
+  constructor(username, email, password, passwordHash) {
     this.username = username;
     this.email = email;
     this.password = password;
+    this.passwordHash = passwordHash;
   }
 
-  static validade(body) {
+  static async validade(body) {
     const errors = [];
 
-    if (!body.username) errors.push('username can not be empty');
-    if (!body.email) errors.push('email can not be empty');
-    if (!body.password) errors.push('password can not be empty');
+    if (!body.username) errors.push("username can not be empty");
+    if (!body.email) errors.push("email can not be empty");
+    if (!body.password) errors.push("password can not be empty");
+
+    // Precisa testar essa parte de hash
+
+    if (body.password) {
+      User.passwordHash = await bcrypt.hash(User.password, 8);
+    }
+
+    User.prototype.checkPassword = function(password) {
+      return bcrypt.compare(password, this.passwordHash);
+    };
 
     return errors;
   }
-  /* , {
-        hooks: {
-            beforeSave: async user => {
-                if (user.password) {
-                    user.password = await bcrypt.hash(user.password, 8)
-                }
-            }
-        }
-    }
-  */
 }
 
 module.exports = User;
