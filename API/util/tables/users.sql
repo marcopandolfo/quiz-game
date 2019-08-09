f434728da9456d7a5a909546658d13a8ecc91480
m7Aei <mateuspm2015@hotmail.com>
2019-08-09 14:08:08 -0300

    Alterando a tabela de users

--- API/util/tables/users.sql
+++ API/util/tables/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`id` int(10) NOT NULL auto_increment,
 	`username` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
 	`passwordHash` varchar(255) NOT NULL,
 	PRIMARY KEY( `id` )
 );
